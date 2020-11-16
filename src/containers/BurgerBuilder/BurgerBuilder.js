b629b58e6beac36507a126334fe7320bcced0e11
Justin <weiwenl.justin@gmail.com>
2020-11-16 18:10:22 +0800

    navigate to checkout page

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -38,29 +38,30 @@ class BurgerBuilder extends Component {
 
     purchaseContinueHandler = () => {
         // alert('Continue to payment!');
-        this.setState({'loading': true});
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Justin Lee',
-                address: {
-                    street: 'Lorong Chuan',
-                    zipcode: '98765',
-                    country: 'Singapore'
-                },
-                email: 'test@gmail.com'
-            },
-            deliveryMethod: 'fastest'
+        // this.setState({'loading': true});
+        // const order = {
+        //     ingredients: this.state.ingredients,
+        //     price: this.state.totalPrice,
+        //     customer: {
+        //         name: 'Justin Lee',
+        //         address: {
+        //             street: 'Lorong Chuan',
+        //             zipcode: '98765',
+        //             country: 'Singapore'
+        //         },
+        //         email: 'test@gmail.com'
+        //     },
+        //     deliveryMethod: 'fastest'
             
-        }
-        axios.post('/orders.json', order)
-        .then(response => {
-            this.setState({loading: false, purchasing: false});
-        })
-        .catch(error => {
-            this.setState({loading: false, purchasing: false});
-        });
+        // }
+        // axios.post('/orders.json', order)
+        // .then(response => {
+        //     this.setState({loading: false, purchasing: false});
+        // })
+        // .catch(error => {
+        //     this.setState({loading: false, purchasing: false});
+        // });
+        this.props.history.push('/checkout');
     }
 
     purchaseCancelHandler = () => {
